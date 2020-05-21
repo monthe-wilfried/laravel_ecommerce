28c35c66b651ccdec6b4b3c7eb78fdb82f7bd07f
Monthe Wilfried <monthetcham@yahoo.fr>
2020-05-21 03:48:46 +0200

    Admin Dashboard Charts Dynamic Done!

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,6 +2,21 @@
 
 @section('admin_content')
 
+    @php
+        $date = date('d-m-y');
+        $month = date('F');
+        $year = date('Y');
+        $today_amount = \App\Order::where('date', $date)->sum('total');
+        $month_amount = \App\Order::where('month', $month)->sum('total');
+        $year_amount = \App\Order::where('year', $year)->sum('total');
+        $delivery = \App\Order::where('status',3)->sum('total');
+
+        $return_amount = \App\Order::where('return_order',2)->sum('total');
+        $products = \App\Product::all();
+        $brands = \App\Brand::all();
+        $users = \App\User::all();
+    @endphp
+
     <!-- ########## START: MAIN PANEL ########## -->
     <div class="sl-mainpanel">
         <nav class="breadcrumb sl-breadcrumb">
@@ -15,69 +30,26 @@
                 <div class="col-sm-6 col-xl-3">
                     <div class="card pd-20 bg-primary">
                         <div class="d-flex justify-content-between align-items-center mg-b-10">
-                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Today's Sales</h6>
-                            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
+                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Today's Order</h6>
                         </div><!-- card-header -->
                         <div class="d-flex align-items-center justify-content-between">
                             <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
-                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">$850</h3>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">${{ $today_amount }}</h3>
                         </div><!-- card-body -->
-                        <div class="d-flex align-items-center justify-content-between mg-t-15 bd-t bd-white-2 pd-t-10">
-                            <div>
-                                <span class="tx-11 tx-white-6">Gross Sales</span>
-                                <h6 class="tx-white mg-b-0">$2,210</h6>
-                            </div>
-                            <div>
-                                <span class="tx-11 tx-white-6">Tax Return</span>
-                                <h6 class="tx-white mg-b-0">$320</h6>
-                            </div>
-                        </div><!-- -->
+
                     </div><!-- card -->
                 </div><!-- col-3 -->
 
                 <div class="col-sm-6 col-xl-3 mg-t-20 mg-sm-t-0">
                     <div class="card pd-20 bg-info">
                         <div class="d-flex justify-content-between align-items-center mg-b-10">
-                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">This Week's Sales</h6>
-                            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
-                        </div><!-- card-header -->
-                        <div class="d-flex align-items-center justify-content-between">
-                            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
-                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">$4,625</h3>
-                        </div><!-- card-body -->
-                        <div class="d-flex align-items-center justify-content-between mg-t-15 bd-t bd-white-2 pd-t-10">
-                            <div>
-                                <span class="tx-11 tx-white-6">Gross Sales</span>
-                                <h6 class="tx-white mg-b-0">$2,210</h6>
-                            </div>
-                            <div>
-                                <span class="tx-11 tx-white-6">Tax Return</span>
-                                <h6 class="tx-white mg-b-0">$320</h6>
-                            </div>
-                        </div><!-- -->
-                    </div><!-- card -->
-                </div><!-- col-3 -->
-
-                <div class="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
-                    <div class="card pd-20 bg-purple">
-                        <div class="d-flex justify-content-between align-items-center mg-b-10">
                             <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">This Month's Sales</h6>
-                            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
                         </div><!-- card-header -->
                         <div class="d-flex align-items-center justify-content-between">
                             <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
-                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">$11,908</h3>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">${{ $month_amount }}</h3>
                         </div><!-- card-body -->
-                        <div class="d-flex align-items-center justify-content-between mg-t-15 bd-t bd-white-2 pd-t-10">
-                            <div>
-                                <span class="tx-11 tx-white-6">Gross Sales</span>
-                                <h6 class="tx-white mg-b-0">$2,210</h6>
-                            </div>
-                            <div>
-                                <span class="tx-11 tx-white-6">Tax Return</span>
-                                <h6 class="tx-white mg-b-0">$320</h6>
-                            </div>
-                        </div><!-- -->
+
                     </div><!-- card -->
                 </div><!-- col-3 -->
 
@@ -85,22 +57,77 @@
                     <div class="card pd-20 bg-sl-primary">
                         <div class="d-flex justify-content-between align-items-center mg-b-10">
                             <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">This Year's Sales</h6>
-                            <a href="" class="tx-white-8 hover-white"><i class="icon ion-android-more-horizontal"></i></a>
                         </div><!-- card-header -->
                         <div class="d-flex align-items-center justify-content-between">
                             <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
-                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">$91,239</h3>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">${{ $year_amount }}</h3>
                         </div><!-- card-body -->
-                        <div class="d-flex align-items-center justify-content-between mg-t-15 bd-t bd-white-2 pd-t-10">
-                            <div>
-                                <span class="tx-11 tx-white-6">Gross Sales</span>
-                                <h6 class="tx-white mg-b-0">$2,210</h6>
-                            </div>
-                            <div>
-                                <span class="tx-11 tx-white-6">Tax Return</span>
-                                <h6 class="tx-white mg-b-0">$320</h6>
-                            </div>
-                        </div><!-- -->
+                    </div><!-- card -->
+                </div><!-- col-3 -->
+
+                <div class="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
+                    <div class="card pd-20 bg-purple">
+                        <div class="d-flex justify-content-between align-items-center mg-b-10">
+                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Delivered Sales</h6>
+                        </div><!-- card-header -->
+                        <div class="d-flex align-items-center justify-content-between">
+                            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">${{ $delivery }}</h3>
+                        </div><!-- card-body -->
+                    </div><!-- card -->
+                </div><!-- col-3 -->
+            </div><!-- row -->
+            <br><br>
+
+
+            <div class="row row-sm">
+                <div class="col-sm-6 col-xl-3">
+                    <div class="card pd-20 bg-danger">
+                        <div class="d-flex justify-content-between align-items-center mg-b-10">
+                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Returned Sales</h6>
+                        </div><!-- card-header -->
+                        <div class="d-flex align-items-center justify-content-between">
+                            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">${{ $return_amount }}</h3>
+                        </div><!-- card-body -->
+
+                    </div><!-- card -->
+                </div><!-- col-3 -->
+
+                <div class="col-sm-6 col-xl-3 mg-t-20 mg-sm-t-0">
+                    <div class="card pd-20 bg-warning">
+                        <div class="d-flex justify-content-between align-items-center mg-b-10">
+                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Total Products</h6>
+                        </div><!-- card-header -->
+                        <div class="d-flex align-items-center justify-content-between">
+                            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ count($products) }}</h3>
+                        </div><!-- card-body -->
+
+                    </div><!-- card -->
+                </div><!-- col-3 -->
+
+                <div class="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
+                    <div class="card pd-20 bg-indigo">
+                        <div class="d-flex justify-content-between align-items-center mg-b-10">
+                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Total Brands</h6>
+                        </div><!-- card-header -->
+                        <div class="d-flex align-items-center justify-content-between">
+                            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ count($brands) }}</h3>
+                        </div><!-- card-body -->
+                    </div><!-- card -->
+                </div><!-- col-3 -->
+
+                <div class="col-sm-6 col-xl-3 mg-t-20 mg-xl-t-0">
+                    <div class="card pd-20 bg-dark">
+                        <div class="d-flex justify-content-between align-items-center mg-b-10">
+                            <h6 class="tx-11 tx-uppercase mg-b-0 tx-spacing-1 tx-white">Total Users</h6>
+                        </div><!-- card-header -->
+                        <div class="d-flex align-items-center justify-content-between">
+                            <span class="sparkline2">5,3,9,6,5,9,7,3,5,2</span>
+                            <h3 class="mg-b-0 tx-white tx-lato tx-bold">{{ count($users) }}</h3>
+                        </div><!-- card-body -->
                     </div><!-- card -->
                 </div><!-- col-3 -->
             </div><!-- row -->
